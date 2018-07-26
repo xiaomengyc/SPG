adec5ca5999ac0c68d99176cd4f6e5566ad3485d
Xiaolin Zhang <solli.zhang@gmail.com>
2018-07-26 12:19:44 -0500

    changed scripts

--- scripts/train_imagenet_full_v5.sh
+++ scripts/train_imagenet_full_v5.sh
@@ -2,18 +2,18 @@
 
 cd ../exper/
 
-#CUDA_VISIBLE_DEVICES=0 python train_vgg_imagenet_new_frame.py --arch=vgg_v5 --epoch=26 --lr=0.001 --batch_size=20 --num_gpu=1 --dataset=imagenet  \
-#	--snapshot_dir=../snapshots/vgg16_imagenet_v5/  \
-#	--restore_from=/home/xiaolin/.torch/models/vgg16-397923af.pth \
 
 ROOT_DIR=/home/xiaolin/eccv18
 
-#CUDA_VISIBLE_DEVICES=0,1 python train_vgg_imagenet_new_frame.py --arch=vgg_v5_3_1 --epoch=26 --lr=0.001 --batch_size=20 --num_gpu=2 --dataset=imagenet  \
-CUDA_VISIBLE_DEVICES=0,1 python train_vgg_imagenet_new_frame.py --arch=vgg_v5_4 --epoch=26 --lr=0.0001 --batch_size=20 --num_gpu=2 --dataset=imagenet  \
+
+CUDA_VISIBLE_DEVICES=0,1 python train_frame.py 
+	--arch=inceptionv3_spg --epoch=5 --lr=0.001 --batch_size=30 
+	--num_gpu=2 --dataset=imagenet  \
 	--img_dir=${ROOT_DIR}/data/ILSVRC/Data/CLS-LOC/train \
 	--train_list=${ROOT_DIR}/data/ILSVRC/list/train_list.txt \
 	--num_classes=1000 \
-	--snapshot_dir=../snapshots/vgg16_imagenet_full_v5_4/  \
+	--snapshot_dir=../snapshots/inception_imagenet_full_spg/  \
+	
+	
 	#--restore_from=/home/xiaolin/.torch/models/vgg16-397923af.pth \
 
-	#--snapshot_dir=../snapshots/vgg16_imagenet_full_v5_4/  \
--- scripts/val_imagenet_full.sh
+++ scripts/val_imagenet_full.sh
@@ -6,7 +6,8 @@ cd ../exper/
 #v5_v3
 
 ROOT_DIR=/home/xiaolin/eccv18
-CUDA_VISIBLE_DEVICES=1 python val_vgg_imagenet_single_img.py --arch=vgg_v5_4 \
+
+CUDA_VISIBLE_DEVICES=1 python val_frame.py --arch=inceptionv3_spg \
 	--batch_size=1 \
 	--img_dir=${ROOT_DIR}/data/ILSVRC/Data/CLS-LOC/val \
 	--train_list=${ROOT_DIR}/data/ILSVRC/list/train_list.txt \
@@ -14,6 +15,5 @@ CUDA_VISIBLE_DEVICES=1 python val_vgg_imagenet_single_img.py --arch=vgg_v5_4 \
 	--num_gpu=1 \
 	--dataset=imagenet \
 	--num_classes=1000 \
-	--snapshot_dir=../snapshots/vgg16_imagenet_full_v5_4/
+	--snapshot_dir=../snapshots/inception_imagenet_full_spg/
 
-#	--img_dir=${ROOT_DIR}/data/ILSVRC/Data/CLS-LOC/train \

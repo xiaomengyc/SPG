792c064c5d9fa144cf26b6d0e1d4f0a5b1a2c248
Xiaolin Zhang <solli.zhang@gmail.com>
2018-09-13 18:56:05 +1000

    correct a mistake in val script

--- scripts/val_imagenet_full.sh
+++ scripts/val_imagenet_full.sh
@@ -7,7 +7,7 @@ cd ../exper/
 
 ROOT_DIR=/home/xiaolin/eccv18
 
-CUDA_VISIBLE_DEVICES=1 python val_frame.py --arch=inceptionv3_spg \
+CUDA_VISIBLE_DEVICES=1 python val_frame.py --arch=inception3_spg \
 	--batch_size=1 \
 	--img_dir=${ROOT_DIR}/data/ILSVRC/Data/CLS-LOC/val \
 	--train_list=${ROOT_DIR}/data/ILSVRC/list/train_list.txt \
